c9698a6ae0a27a661f6b5d192a172939eac32759
Julien Vely <jvely@paypal.com>
2014-12-10 17:26:51 +0800

    Creating the project

---
+++ src/test/java/com/symbio/test/Mytest1.java
@@ -0,0 +1,31 @@
+package com.symbio.test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Mytest1 {
+	
+	@Test
+	public void test() throws InterruptedException{
+		
+		WebDriver driver =  new FirefoxDriver();
+		String url = "http://www.amazon.ca";
+		driver.get(url);
+		driver.findElement(By.cssSelector("#twotabsearchtextbox")).sendKeys("iphone 6");
+		driver.findElement(By.cssSelector(".nav-submit-input")).click();
+		Thread.sleep(10000);
+		driver.findElement(By.cssSelector("#result_0 a")).click();
+		for(String winHandle : driver.getWindowHandles())
+		    driver.switchTo().window(winHandle);
+		Thread.sleep(10000);
+		driver.findElement(By.cssSelector("#add-to-cart-button")).click();
+		Thread.sleep(10000);
+		driver.findElement(By.cssSelector("#nav-cart")).click();
+		Thread.sleep(10000);
+		driver.findElement(By.cssSelector("#a-autoid-0-announce")).click();
+		Thread.sleep(10000);
+		driver.findElement(By.cssSelector("#dropdown1_2")).click();		
+	}
+ }
